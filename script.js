b24b08370b2dca4064344474c5eb7df5d6d746b7
Leandro Cardoso <125698129+LeandroSCardoso@users.noreply.github.com>
2023-03-15 17:45:55 -0300

    FALTA SÓ API
    ESTA ENVIADNO, FALTA MONTAR API

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/* //load ao carregar a tela
+window.addEventListener("load",function() {
+  //pegando o botão que vai disparar o envio para depois disparara a função de envio
+  document.getElementById("enviar").addEventListener("click", EnviaFormulario())
+}) */
+
+
+
+function EnviaFormulario(){
+  console.log("VAIIIIII FELADAPUTA")  
+}
