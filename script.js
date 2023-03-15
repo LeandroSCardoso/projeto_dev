9c4ca1eef7908ec6fdc214e6e33556ae7a9c5668
Leandro Cardoso <125698129+LeandroSCardoso@users.noreply.github.com>
2023-03-15 19:32:40 -0300

    Envio FInal
    Envio final, depois farei apenas o extra que é a validação.

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 
-/* //load ao carregar a tela
-window.addEventListener("load",function() {
-  //pegando o botão que vai disparar o envio para depois disparara a função de envio
-  document.getElementById("enviar").addEventListener("click", EnviaFormulario())
-}) */
+/* 
+  SCRIPTS PARA ENVIO DE FORMULÁRIO DO PROJETO DEV
+*/
 
 
+async function EnviaFormulario(){
 
-function EnviaFormulario(){
-  console.log("VAIIIIII FELADAPUTA")  
-}
+
+    // Entradas
+
+    const input_nome = document.querySelector("#nome").value
+    const input_email = document.querySelector("#email").value
+    const input_texto = document.querySelector("#texto").value
+   
+    // Chamada pra API
+    const url = "https://target-api-simples.cyclic.app/generica"
+    const fetchOptions = {
+      method: "POST",
+      body: JSON.stringify({
+        nome: input_nome,
+        email: input_email,
+        texto: input_texto
+      }),
+      // body: `{"title": ${inputTitleValue}, "description": ${inputDescriptionValue}}`,
+      headers: {"Content-type": "application/json"}
+    }
+    const response = await fetch(url, fetchOptions)
+    const responseData = await response.json()
+
+    // Elemento que vamos mostrar o retorno da api
+    const nome_resp = document.querySelector("#nome_resp")
+    const email_resp = document.querySelector("#email_resp")
+    const texto_resp = document.querySelector("#texto_resp")
+    const retorno = document.querySelector("#retorno")
+    const aviso = document.querySelector("#aviso")
+    
+    //mostando divs de avisos
+    aviso.style.display = 'inline-block'
+    retorno.style.display = 'inline-block'
+
+    //passando os valores enviados para os campos
+    nome_resp.innerHTML = JSON.stringify(responseData.apiRecebeu.nome)
+    email_resp.innerHTML = JSON.stringify(responseData.apiRecebeu.email)
+    texto_resp.innerHTML = JSON.stringify(responseData.apiRecebeu.texto)
+  }
